8cf31db58e921c7e4778691c21cdb59251b5e8cc
Sean Mullen <srmullen13@gmail.com>
2021-04-21 10:28:37 -0400

    counter page

---
+++ src/routes/counter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Counter from '$lib/Counter/index.svelte';
+</script>
+
+<section>
+  <Counter />
+</section>
+
+<style>
+  section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,18 +3,15 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter/index.svelte';
 	import Pricing from '$lib/Pricing/index.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Pricing</title>
 </svelte:head>
 
 <section>
 	<Pricing />
-
-	<!-- <Counter /> -->
 </section>
 
 <style>
@@ -24,24 +21,5 @@
 		justify-content: center;
 		align-items: center;
 		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
