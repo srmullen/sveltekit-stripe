c57da8cd5af99131a1cc2e641847716226cf60ef
Sean Mullen <srmullen13@gmail.com>
2021-05-12 08:44:41 -0400

    update to latest sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
-const sveltePreprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-node');
-const pkg = require('./package.json');
+import sveltePreprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: sveltePreprocess(),
@@ -15,12 +14,8 @@ module.exports = {
 		adapter: node(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
+		target: '#svelte'
 	}
 };
+
+export default config;
